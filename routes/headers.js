e4c862d387e151dc1e342b71261a41415eacc510
Cody Green <cody@codygreen.com>
2016-10-18 16:29:54 -0500

    Initial Check in of demo site
    demo site for header authentication

---
+++ routes/headers.js
@@ -0,0 +1,15 @@
+var express = require('express');
+var router = express.Router();
+
+/* GET home page. */
+router.get('/', function(req, res, next) {
+  res.render('headers', 
+  	{ 
+  		title: 'Express',
+  		user: req.get('X-User'),
+  		headers: req.headers, 
+  		headersSize: Object.keys(req.headers).length
+  	});
+});
+
+module.exports = router;
